7e127a2466f9f9778c7a4b9e561e58cd582530e0
Focus Sila Pakdeewong <53871704+MisterFocusTH@users.noreply.github.com>
2020-05-26 14:56:52 +0700

    Add SwipeRefreshLayout / Add Internet Connection Check / Hardcoded Fix

--- app/src/main/java/com/misterfocusth/covid19tracker/MainActivity.java
+++ app/src/main/java/com/misterfocusth/covid19tracker/MainActivity.java
@@ -2,8 +2,14 @@ package com.misterfocusth.covid19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,17 +23,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.InetAddress;
+
 // Made By Focus Sila Pakdeewong (MisterFocusTH)
 // Thanks API & Data From : Open Government Data of Thailand and Ministry of Public Health (Thailand)
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
 
     private TextView textUpdatedDate , textNewCases , textNewRecovered , textNewDeath , textTodayUpdate; // TextViews UI Components
     private Button btnReportIssues , btnOpenBrowser; // Button UI Components
+    private SwipeRefreshLayout mSwipeRefreshLayout; // SwipeRefreshLayout UI Component
+    private View view; // View UI Component
 
     private static final String API_URL = "https://covid19.th-stat.com/api/open/today";
     private static final String OFFICIAL_URL = "https://covid19.ddc.moph.go.th/";
@@ -43,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // UI Components - View
+        view = findViewById(R.id.constrainLayout);
+
         // UI Components - TextViews
         textUpdatedDate = findViewById(R.id.textView_updatedDate);
         textNewCases = findViewById(R.id.textView_newcases);
@@ -56,6 +70,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnOpenBrowser = findViewById(R.id.btn_viewOnBrowser);
         btnOpenBrowser.setOnClickListener(MainActivity.this);
+
+        // UI Components - SwipeRefresLayout
+        mSwipeRefreshLayout = findViewById(R.id.pullToRefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(MainActivity.this);
+
+        checkInternetConnection(MainActivity.this);
 
         //Request Data From API_URL
         RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
@@ -87,6 +107,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 });
         requestQueue.add(jsonObjectRequest);
+
+        Snackbar.make(view, getResources().getString(R.string.snackbar_text), Snackbar.LENGTH_LONG);
     }
 
     @Override
@@ -105,6 +127,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onRefresh() {
+        recreate();
+    }
+
     private void replaceData() {
 
         textViewData[0] = getResources().getString(R.string.box_updatedDate);
@@ -113,14 +140,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewData[3] = getResources().getString(R.string.box_death);
         textViewData[4] = getResources().getString(R.string.box_today_update);
 
-        replacedData[0] = textViewData[0].replaceAll("replace", receivedData[0]); // Updated Date
+        replacedData[0] = textViewData[0].replaceAll("date", receivedData[0]); // Updated Date
         replacedData[1] = textViewData[1].replaceAll("_", receivedData[1]); // Update NewCases
         replacedData[2] = textViewData[2].replaceAll("_", receivedData[2]); // Update Recovered
         replacedData[3] = textViewData[3].replaceAll("_", receivedData[3]); // Update NewDeath
 
         String replacedString = "";
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             if (i == 0) {
                 replacedString = textViewData[4].replaceAll("newCase", receivedData[1]);
                 i++;
@@ -137,6 +164,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 replacedString = replacedString.replaceAll("newDeath", receivedData[3]);
                 i++;
             }
+            if (i == 4) {
+                replacedString = replacedString.replaceAll("date", receivedData[0]);
+                i++;
+            }
         }
 
         replacedData[4] = replacedString; // Text : Today Update Box
@@ -148,5 +179,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textNewRecovered.setText(replacedData[2]); // Update : New Recovered TextViews
         textNewDeath.setText(replacedData[3]); // Update : New Death TextViews
         textTodayUpdate.setText(replacedData[4]); // Update : Today TextViews
+    }
+
+    private void checkInternetConnection(Context context) {
+        if (!isInternetAvailable()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(getResources().getString(R.string.dialog_no_internet_title))
+                    .setMessage(getResources().getString(R.string.dialog_no_internet_message))
+                    .setPositiveButton(getResources().getString(R.string.dialog_no_internet_button), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    });
+            builder.show();
+        }
+    }
+
+    private Boolean isInternetAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            if (activeNetworkInfo != null) {
+                if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                    return true;
+                } else if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return  false;
+        }
+        return false;
     }
 }
